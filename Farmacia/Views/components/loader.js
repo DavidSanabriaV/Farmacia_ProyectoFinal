9933736e756a52ce912dda5e30c51312005e091c
Santiago Orozco <sorozco80826@ufide.ac.cr>
2026-04-08 15:30:48 -0600

    Cambios en index y categorias
    Implementación del navbar y footer en index y categorias

--- Farmacia/Views/components/loader.js
+++ Farmacia/Views/components/loader.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const depth = window.location.pathname.split("/").length - 2;
-    const prefix = depth > 1 ? "../" : "./";
+    const base = "/Farmacia/Views/";
 
-    fetch(prefix + "components/navbar.html")
+    fetch(base + "components/navbar.html")
         .then(res => res.text())
         .then(html => {
             document.getElementById("navbar-placeholder").innerHTML = html;
         });
 
-    fetch(prefix + "components/footer.html")
+    fetch(base + "components/footer.html")
         .then(res => res.text())
         .then(html => {
             document.getElementById("footer-placeholder").innerHTML = html;
